70c43b26f5ccd9dbed35572d6a8a49d369d3befe
shafiulmmc <shafiul.primitivesolution@gmail.com>
2021-07-31 00:35:36 +0600

    complated todo-list-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ChangeEvent } from 'react';
+import { useState } from 'react';
 import './App.css';
+import TodoTask from './Components/TodoTask';
+import {ITask} from './Interfaces';
 
 function App() {
+
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>)=>{
+    if(e.target.name === "task"){
+      setTask(e.target.value)
+    }else{
+      setDeadline(Number(e.target.value));
+    }
+  };
+
+  const addTask = () =>{
+    const newTask = {taskName: task, deadline: deadline};
+    setTodoList([...todoList, newTask ]);
+    setTask("");
+    setDeadline(0);
+  };
+
+  const complateTask = (taskNameToDelete: string) =>{
+    setTodoList(todoList.filter((task)=>{
+      return task.taskName !== taskNameToDelete;
+    }))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="inputContainer">
+          <input type="text" placeholder="Task..." value={task} name="task" onChange={handleChange} />
+          <input type="number" placeholder="Deadline (in Days)" value={deadline} name="deadline" onChange={handleChange} />
+        </div>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="todoList">
+        {
+          todoList.map((task: ITask, key: number)=>{
+            return <TodoTask key={key} task={task} complateTask={complateTask}></TodoTask>
+          })
+        }
+      </div>
     </div>
   );
 }
